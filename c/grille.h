101c8508255af890ee1e65d09ec3d066036394f7
Clément Leboulenger <clement.leboulenger@etu.sorbonne-universite.fr>
2019-11-27 09:35:46 +0100

    Ajout definitions fonctions

--- c/grille.h
+++ c/grille.h
@@ -1,1 +1,9 @@
+#ifndef __GRILLE_H__
+#define __GRILLE_H__
 #define N 10
+
+void poseBateau(int grille[N][N], int taille, int nbBateau);
+void initGrille(int grille[N][N]);
+void afficheGrille(int grille[N][N]);
+
+#endif
