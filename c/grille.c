bb57dbd1d576d26568e826190dc4dd1fb5a81a07
ClementRoger <clementrger@gmail.com>
2019-11-27 09:29:58 +0100

    ajoutfonctions posebateau

--- c/grille.c
+++ c/grille.c
@@ -11,7 +11,9 @@ void initGrille(int grille[N][N]){
 			grille[e][f] = 0;
 		}
 	}
+
 }
+
 
 void afficheGrille(int grille[N][N]){
 
@@ -29,11 +31,31 @@ void afficheGrille(int grille[N][N]){
 				
 			}
 
-			else{
+			else if(grille[ligne][colonne] == 4){
 				printf("¤|");
 
 			}
+
+			else if(grille[ligne][colonne] == 1){
+				printf("#|");
+
+			}
 			
+			else if(grille[ligne][colonne] == 2){
+				printf("O|");
+
+			}
+
+			else if(grille[ligne][colonne] == 3){
+				printf("$|");
+
+			}
+
+			else if(grille[ligne][colonne] == 5){
+				printf("@|");
+
+			}
+
 		} 
 
 	printf("\n");
@@ -43,35 +65,76 @@ void afficheGrille(int grille[N][N]){
 	
 }
 
-/*  1 porte-avions (5 cases) ----> rempli avec 1
-    1 croiseur (4 cases) -----> rempli avec 2
+/*  1 porte-avions (5 cases) ----> rempli avec 5
+    1 croiseur (4 cases) -----> rempli avec 4
     1 contre-torpilleurs (3 cases) -----> rempli avec 3
-    1 contre-torpilleurs (3 cases) -----> rempli avec 4
-    1 torpilleur (2 cases) -----> rempli avec 5 */
+    1 contre-torpilleurs (3 cases) -----> rempli avec 3
+    1 torpilleur (2 cases) -----> rempli avec 2 */
 
+
+void poseBateau(int grille[N][N], int taille, int nombre_bateau){
+
+	static int numBateau = 1;
+
+	/* Installation des porte avions */
+
+	for(int i = 0; i<nombre_bateau;i++){
+
+		int x, y, orientation;
+		int flag = 0;
+
+		while(!flag){
+
+			x = (int)(rand() / (double)RAND_MAX * (N - 1));
+			y = (int)(rand() / (double)RAND_MAX * (N - 1));
+			orientation = (int)(rand() / (double)RAND_MAX * 2);
+
+			if(orientation==1){ // horizontal
+				if(x <= N - taille){
+					flag = 1;
+					for(int e = x; e<(x+taille); e++){ //check si les cases sont vides
+						if(grille[e][y]!=0){
+							flag=0;
+						}
+					}
+				}
+			} else {
+				if(y <= N - taille){
+					flag = 1;
+					for(int e = y; e<(y+taille); e++){ //check si les cases sont vides
+						if(grille[x][e]!=0){
+							flag=0;
+						}
+					}
+				}
+
+			}
+		}
+
+		if(orientation == 0){ //vertical
+			for(int p=0; p<taille;p++){
+				grille[x][y+p] = numBateau;
+			}
+		} else {
+			for(int p=0; p<taille;p++){
+				grille[x+p][y] = numBateau;
+			}
+
+		}
+	numBateau++;
+	}
+}
 
 void remplitGrille(int grille[N][N]){
 
-	int e = 1;
-
-	while(e<=5){
-
-	int i = (int)(rand() / (double)RAND_MAX * (N - 1));
-	int j = (int)(rand() / (double)RAND_MAX * (N - 1));
-
-	if(grille[i][j] == 0){ //Pour pas que les bateaux soient au même endroit
-		grille[i][j] = e;
-		printf("%d  %d\n",i,j);
-		e++; }
-
-	else{
-
-		int i = (int)(rand() / (double)RAND_MAX * (N - 1));
-		int j = (int)(rand() / (double)RAND_MAX * (N - 1));
-
-		}
-	}
+	poseBateau(grille,5,1);
+	poseBateau(grille,4,1);
+	poseBateau(grille,3,2);
+	poseBateau(grille,2,1);
 }
+		
+
+
 
 
 int main(){
@@ -81,6 +144,4 @@ int main(){
 	initGrille(grille);
 	remplitGrille(grille);
 	afficheGrille(grille);
-
-
 }
