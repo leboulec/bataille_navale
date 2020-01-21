0d458a1346f22a1560d223c3c13b42a1bce4cb63
Clément Leboulenger <clement.leboulenger@etu.upmc.fr>
2020-01-21 14:31:42 +0100

    Ajout d'un message d'erreur quand le caractère rentré n'est pas un entier

--- mips/bataille_navale.asm
+++ mips/bataille_navale.asm
@@ -4,7 +4,7 @@ bord_grille: 	.asciiz "___________________________________________\n"
 numColonne:		.asciiz "|.| 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |\n"
 separateur:		.asciiz " |"
 nom_fichier:    .asciiz "score.txt"
-
+erreur_lecture: .asciiz "\nErreur : veuillez rentrer une valeur correcte\n"
 
 	.text
 
@@ -350,13 +350,18 @@ tableau_score:
 lecture_entier:
 			ori $t0, $zero, 47			# Code ASCII 0 - 1
 			ori $t1, $zero, 58			# Code ASCII 9 + 1
+			j while_lect_entier
+lecture_entier_erreur:
+			la $a0, erreur_lecture
+			ori $v0, $zero, 4
+			syscall
 while_lect_entier:
 			ori $v0, $zero, 12
 			syscall
 			slt $t2, $t0, $v0
 			slt $t3, $v0, $t1
 			and $t2, $t2, $t3			# Est-ce que le caractère rentré correspond à un nombre?
-			beq $t2, $zero, while_lect_entier
+			beq $t2, $zero, lecture_entier_erreur
 
 			subi $v0, $v0, 48			# Conversion caractère -> entier
 
--- mips/test_lecture_entier.asm
+++ mips/test_lecture_entier.asm
@@ -1,4 +1,5 @@
 .data
+erreur_lecture: .asciiz "\nErreur : veuillez rentrer une valeur correcte\n"
 .text
 main:
 			jal lecture_entier
@@ -11,13 +12,18 @@ main:
 lecture_entier:
 			ori $t0, $zero, 47			# Code ASCII 0 - 1
 			ori $t1, $zero, 58			# Code ASCII 9 + 1
+			j while_lect_entier
+lecture_entier_erreur:
+			la $a0, erreur_lecture
+			ori $v0, $zero, 4
+			syscall
 while_lect_entier:
 			ori $v0, $zero, 12
 			syscall
 			slt $t2, $t0, $v0
 			slt $t3, $v0, $t1
 			and $t2, $t2, $t3			# Est-ce que le caractère rentré correspond à un nombre?
-			beq $t2, $zero, while_lect_entier
+			beq $t2, $zero, lecture_entier_erreur
 
 			subi $v0, $v0, 48			# Conversion caractère -> entier
 
